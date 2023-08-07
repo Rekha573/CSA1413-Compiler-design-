350c56aeed1c775841d32a0392474a1614e231f4
Rekha <113973508+Rekha573@users.noreply.github.com>
2023-08-07 10:25:12 +0530

    Create identifier.comp.c

---
+++ identifier.comp.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+#include<ctype.h>
+
+int main(){
+
+	char a[10];
+
+	int flag,i=1;
+
+	printf("\n enter an identifier: ");
+
+	gets(a);
+
+	if(isalpha(a[0]))
+
+	flag=1;
+
+	else
+
+	flag=0;
+
+	if (flag==1)
+
+	printf("\n valid identifier");
+
+	else
+
+	printf("\n not an identifier");
+
+}
